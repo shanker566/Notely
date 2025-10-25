a497d62cece71fde2c7f2887699c1b1759e49933
shanker566 <gowrishankerbhonagiri56@gmail.com>
2025-10-25 18:29:53 +0530

    Add Vercel Analytics

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // <-- Make sure this is imported
 import App from './App';
 import './index.css';
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter> {/* <-- Make sure this wrapper is here */}
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );
